7c79e2c7423c9bb4f7cd2bea0c3dd49c0539a273
Dmitry Malashev <dmalashev@gmail.com>
2024-11-07 08:46:42 +0300

    feat: solve the isString task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -37,8 +37,14 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string') {
+    return true;
+  }
+  if (typeof value === 'object' && value !== null) {
+    return typeof value.valueOf() === 'string';
+  }
+  return false;
 }
 
 /**
